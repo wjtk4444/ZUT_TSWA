d501560517a85af169738b58080cc9cda25f105f
wjtk4444 <wjtk4444@gmail.com>
2018-11-05 20:15:16 +0100

    renamed and splitted test class into two; added example tests to each

---
+++ Calculator/CalculatorUnitTest/GUITests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorLogic;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class GUITests
+    {
+        [TestMethod]
+        public void InitialValuesTest()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.AreEqual("0", calculator.displayValue);
+        }
+    }
+}
---
+++ Calculator/CalculatorUnitTest/LogicTests.cs
@@ -0,0 +1,21 @@
+﻿using CalculatorLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class LogicTests
+    {
+        [TestMethod]
+        public void InitialValuesTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(0, calculator.result);
+        }
+    }
+}
